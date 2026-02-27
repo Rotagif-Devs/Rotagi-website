728ecf0f2daac77abab0d29a742dbe85d14e39fe
akwadhana <taiworaji4@gmail.com>
2026-02-27 21:52:37 +0100

    Auth updated

---
+++ app/(auth)/forgotpassword/page.tsx
@@ -0,0 +1,10 @@
+import ForgetPassword from '@/components/forgotpassword/ForgotPassword'
+import React from 'react'
+
+const page = () => {
+  return (
+    <ForgetPassword />
+  )
+}
+
+export default page
