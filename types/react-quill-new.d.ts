70296405eb107060d0a3a7dc235a13af621961a4
mansardauna <olaomansur@gmail.com>
2026-05-08 09:59:13 +0100

    build-error-fix

--- types/react-quill-new.d.ts
+++ types/react-quill-new.d.ts
@@ -1,1 +1,25 @@
-declare module 'react-quill-new';
+import { Component } from 'react';
+
+declare module 'react-quill-new' {
+  export interface ReactQuillProps {
+    value?: string;
+    defaultValue?: string;
+    readOnly?: boolean;
+    placeholder?: string;
+    modules?: any;
+    formats?: string[];
+    theme?: string;
+    onChange?: (content: string, delta: any, source: any, editor: any) => void;
+    onChangeSelection?: (selection: any, source: any, editor: any) => void;
+    onFocus?: (selection: any, source: any, editor: any) => void;
+    onBlur?: (previousSelection: any, source: any, editor: any) => void;
+    onKeyPress?: (event: any) => void;
+    onKeyDown?: (event: any) => void;
+    onKeyUp?: (event: any) => void;
+    className?: string;
+    style?: React.CSSProperties;
+    children?: React.ReactNode;
+  }
+
+  export default class ReactQuill extends Component<ReactQuillProps> {}
+}
